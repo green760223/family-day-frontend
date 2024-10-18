38d9098c033c0b54667073852fa6c62b858db26c
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-10-18 00:42:29 -0700

    Update Activity component styles to vertically center content and adjust margins

--- src/views/activity/Activity.tsx
+++ src/views/activity/Activity.tsx
@@ -32,39 +32,67 @@ const Activity = () => {
           "--icon-size": "30px",
         }}>
         <Step
-          title='10:00 - 10:30 AM 開幕活動'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>10:00 - 10:30 AM</div>
+              <div className={styles.customTitleRight}>開幕活動</div>
+            </div>
+          }
           description={
-            <div style={{ lineHeight: "1.2" }}>
+            <div style={{ lineHeight: "1.5" }}>
               主持人開場、活力熱身操、長官致詞、家庭日啟動儀式、大合照
             </div>
           }
           icon={<CheckCircleFill />}
         />
+
         <Step
-          title='10:30 - 11:00 AM 舞台表演(一)'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>10:30 - 11:00 AM</div>
+              <div className={styles.customTitleRight}>舞台表演(一)</div>
+            </div>
+          }
           description={
-            <div style={{ lineHeight: "1.2" }}>經典亂打秀、親子互動演出</div>
+            <div style={{ lineHeight: "1.5" }}>經典亂打秀、親子互動演出</div>
           }
           icon={<CheckCircleFill />}
         />
 
         <Step
-          title='11:00 - 11:05 AM 抽獎時間'
-          description={<div style={{ lineHeight: "1.2" }}>第一輪抽獎</div>}
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>11:00 - 11:05 AM</div>
+              <div className={styles.customTitleRight}>抽獎時間(一)</div>
+            </div>
+          }
+          description={<div style={{ lineHeight: "1.5" }}>第一階段抽獎</div>}
           icon={<CheckCircleFill />}
         />
 
         <Step
-          title='11:05 - 11:30 AM 舞台表演(二)'
-          description={<div style={{ lineHeight: "1.2" }}>樂團演出</div>}
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>11:05 - 11:30 AM</div>
+              <div className={styles.customTitleRight}>舞台表演(二)</div>
+            </div>
+          }
+          description={
+            <div style={{ lineHeight: "1.5" }}>樂團帶動唱跳演出</div>
+          }
           icon={<CheckCircleFill />}
         />
 
         <Step
-          title='11:30 - 12:00 PM 動物見面會'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>11:30 - 12:00 PM</div>
+              <div className={styles.customTitleRight}>動物見面會</div>
+            </div>
+          }
           description={
             <Space block direction='vertical'>
-              <div style={{ lineHeight: "1.2" }}>非洲部落 - 草原聚場</div>
+              <div style={{ lineHeight: "1.5" }}>非洲部落 - 草原聚場</div>
               <Button color='primary'>查看地圖</Button>
             </Space>
           }
@@ -72,10 +100,15 @@ const Activity = () => {
         />
 
         <Step
-          title='12:00 / 15:45 PM JC公主的逆襲'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>12:00 / 15:45 PM</div>
+              <div className={styles.customTitleRight}>JC公主的逆襲</div>
+            </div>
+          }
           description={
             <Space block direction='vertical'>
-              <div style={{ lineHeight: "1.2" }}>阿拉伯皇宮 - 魔幻劇場</div>
+              <div style={{ lineHeight: "1.5" }}>阿拉伯皇宮 - 魔幻劇場</div>
               <Button color='primary'>查看地圖</Button>
             </Space>
           }
@@ -83,10 +116,15 @@ const Activity = () => {
         />
 
         <Step
-          title='14:00 - 14:30 PM 耶誕遊行'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>14:00 - 14:30 PM</div>
+              <div className={styles.customTitleRight}>耶誕遊行</div>
+            </div>
+          }
           description={
             <Space block direction='vertical'>
-              <div style={{ lineHeight: "1.2" }}>美國大西部 – 中央廣場</div>
+              <div style={{ lineHeight: "1.5" }}>美國大西部 – 中央廣場</div>
               <Button color='primary'>查看地圖</Button>
             </Space>
           }
@@ -94,10 +132,15 @@ const Activity = () => {
         />
 
         <Step
-          title='14:45 - 15:15 PM 探索秘境叢林'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>14:45 - 15:15 PM</div>
+              <div className={styles.customTitleRight}>探索秘境叢林</div>
+            </div>
+          }
           description={
             <Space block direction='vertical'>
-              <div style={{ lineHeight: "1.2" }}>
+              <div style={{ lineHeight: "1.5" }}>
                 美國大西部 鳥禽方舟 - 探索館
               </div>
               <Button color='primary'>查看地圖</Button>
@@ -107,10 +150,15 @@ const Activity = () => {
         />
 
         <Step
-          title='15:00 - 15:30 PM 舞台表演(三)'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>15:00 - 15:30 PM</div>
+              <div className={styles.customTitleRight}>舞台表演(三)</div>
+            </div>
+          }
           description={
             <Space block direction='vertical'>
-              <div style={{ lineHeight: "1.2" }}>
+              <div style={{ lineHeight: "1.5" }}>
                 氣球魔術演出、大型泡泡互動秀
               </div>
             </Space>
@@ -119,20 +167,30 @@ const Activity = () => {
         />
 
         <Step
-          title='15:30 - 16:00 PM 舞台表演(四)'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>15:30 - 16:00 PM</div>
+              <div className={styles.customTitleRight}>舞台表演(四)</div>
+            </div>
+          }
           description={
             <Space block direction='vertical'>
-              <div style={{ lineHeight: "1.2" }}>樂團帶動唱</div>
+              <div style={{ lineHeight: "1.5" }}>樂團帶動唱</div>
             </Space>
           }
           icon={<CheckCircleFill />}
         />
 
         <Step
-          title='16:00 - 16:20 PM 閉幕活動'
+          title={
+            <div className={styles.stepContent}>
+              <div className={styles.customTitleLeft}>16:00 - 16:20 PM</div>
+              <div className={styles.customTitleRight}>閉幕活動</div>
+            </div>
+          }
           description={
             <Space block direction='vertical'>
-              <div style={{ lineHeight: "1.2" }}>第二輪抽獎、活動圓滿結束</div>
+              <div style={{ lineHeight: "1.5" }}>第二輪抽獎、活動圓滿結束</div>
             </Space>
           }
           icon={<CheckCircleFill />}
