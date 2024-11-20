be53773957b781e8902deff2480e2e5fa600370c
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-11-20 18:13:36 +0800

    Update login component and error handling

--- src/views/signin/SignIn.tsx
+++ src/views/signin/SignIn.tsx
@@ -6,20 +6,48 @@ import Swal from "sweetalert2"
 
 const SignIn = () => {
   const navigate = useNavigate()
+  const MOBILE_LENGTH = 10
+  const MOBILE_TEST = "0912345678"
 
-  const handleLogin = () => {
-    console.log("handleLogin")
+  const onLoginFinished = async (values: any) => {
+    console.log("onLoginFinished triggered", values)
 
-    Swal.fire({
-      icon: "success",
-      title: "報到成功！",
-      text: "歡迎參加2024豐藝集團家庭日",
-      showConfirmButton: false,
-      timer: 2000,
-      timerProgressBar: true,
-    }).then(() => {
-      navigate("/home")
-    })
+    if (!values.mobile || values.mobile.length !== MOBILE_LENGTH) {
+      return Swal.fire({
+        icon: "error",
+        title: "報到失敗！",
+        text: "手機號碼或格式錯誤，請重新輸入或請洽工作人員",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+      })
+    }
+
+    try {
+      if (values.mobile === MOBILE_TEST) {
+        await Swal.fire({
+          icon: "success",
+          title: "報到成功！",
+          text: "歡迎參加2024豐藝集團家庭日",
+          showConfirmButton: false,
+          timer: 2000,
+          timerProgressBar: true,
+        })
+
+        navigate("/home")
+      }
+    } catch (error) {
+      console.error("Unexpected error:", error)
+
+      await Swal.fire({
+        icon: "error",
+        title: "報到失敗！",
+        text: "請重新輸入或請洽工作人員完成報到",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+      })
+    }
   }
 
   return (
@@ -41,10 +69,10 @@ const SignIn = () => {
         <div className={styles.footer}>
           <Form
             layout='horizontal'
+            onFinish={onLoginFinished}
             footer={
               <Button
                 block
-                onClick={handleLogin}
                 shape='rounded'
                 type='submit'
                 size='large'
@@ -58,10 +86,10 @@ const SignIn = () => {
               </Button>
             }>
             <Form.Item
-              name='cellphone'
+              name='mobile'
               label={<MobileOutlined className={styles.prefixIcon} />}
               rules={[{ required: true, message: "手機號碼不能為空" }]}>
-              <Input onChange={console.log} placeholder='請輸入手機號碼' />
+              <Input placeholder='請輸入手機號碼' />
             </Form.Item>
           </Form>
         </div>
