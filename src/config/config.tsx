cbefd560080272d69bd66b5610e848bbf412f69c
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-11-28 23:51:46 +0800

    feat: add environment configuration file and update HTML tags to include environment attributes

---
+++ src/config/config.tsx
@@ -0,0 +1,32 @@
+type ENV = "dev" | "stg" | "prod"
+
+const env = (document.documentElement.dataset.env as ENV) || "stg"
+
+const config = {
+  dev: {
+    baseAPI: "/api",
+    uploadAPI: "",
+    mock: false,
+    cnd: "",
+    mockAPI: "",
+  },
+  stg: {
+    baseAPI: "/api",
+    uploadAPI: "",
+    mock: false,
+    cnd: "",
+    mockAPI: "",
+  },
+  prod: {
+    baseAPI: "",
+    uploadAPI: "",
+    mock: false,
+    cnd: "",
+    mockAPI: "",
+  },
+}
+
+export default {
+  env,
+  ...config[env],
+}
