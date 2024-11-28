7e74ff5ba651b9ed52311abc1d060e1794b99a0e
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-11-29 02:34:42 +0800

    feat: add map image viewer and info alert to Game component

--- src/views/game/Game.tsx
+++ src/views/game/Game.tsx
@@ -1,7 +1,63 @@
-// import React from "react"
+import { useState, useEffect } from "react"
+import { Image, ImageViewer } from "antd-mobile"
+import Swal from "sweetalert2"
 
 const Game = () => {
-  return <div>This is Games</div>
+  const [mapVisible, setMapVisible] = useState(false)
+  // const [popupVisible, setPopupVisible] = useState(true)
+
+  useEffect(() => {
+    Swal.fire({
+      icon: "info",
+      title: "提示",
+      text: "您可以點擊地圖後放大查看位置",
+      showConfirmButton: true,
+      confirmButtonText: "知道了",
+      timer: 3000,
+      timerProgressBar: true,
+    })
+  }, [])
+
+  const handleImageClick = () => {
+    setMapVisible(true)
+    // setMapVisible((prev) => !prev)
+    // console.log("mapVisible", mapVisible)
+  }
+
+  // const handlePopup = () => {
+  //   setPopupVisible(!popupVisible)
+  //   Swal.fire({
+  //     icon: "success",
+  //     title: "報到成功！",
+  //     text: "歡迎參加2024豐藝集團家庭日",
+  //     showConfirmButton: false,
+  //     timer: 2000,
+  //     timerProgressBar: true,
+  //   })
+  // }
+
+  return (
+    <div>
+      <Image src='public/map.jpg' fit='contain' onClick={handleImageClick} />
+
+      <ImageViewer
+        image='public/map.jpg'
+        visible={mapVisible}
+        onClose={() => {
+          setMapVisible(false)
+        }}
+      />
+
+      {/* <Popup
+        visible={popupVisible}
+        onMaskClick={() => {
+          setPopupVisible(false)
+        }}
+        bodyStyle={{ height: "10vh" }}>
+        <div>Hello</div>
+      </Popup> */}
+    </div>
+  )
 }
 
 export default Game
