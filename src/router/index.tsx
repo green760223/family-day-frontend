cf497c3c75d136924f58c70473e7f1dfa0f403fb
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-12-03 22:46:17 +0800

    feat: add NotFound and Error403 components with routing for 404 and 403 errors

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,12 @@
 import { Navigate, createBrowserRouter } from "react-router-dom"
 import SignIn from "../views/signin/SignIn"
+import NotFound from "../views/NotFound"
 import Home from "../views/home/Home"
 import Layout from "../layout/LayoutFC"
 import Activity from "../views/activity/Activity"
 import Ticket from "../views/ticket/Ticket"
 import Game from "../views/game/Game"
+import Error403 from "../views/Error403"
 
 export const router = [
   {
@@ -37,14 +39,18 @@ export const router = [
       },
     ],
   },
-  // {
-  //   path: "*",
-  //   element: <Navigate to='/404' />,
-  // },
-  // {
-  //   path: "/404",
-  //   element: <NotFound />,
-  // },
+  {
+    path: "*",
+    element: <Navigate to='/404' />,
+  },
+  {
+    path: "/404",
+    element: <NotFound />,
+  },
+  {
+    path: '/403',
+    element: <Error403 />
+  }
 ]
 
 export default createBrowserRouter(router)
