edcbe8e1de1b8e4e612cdb28ab69921e20981f94
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-12-10 03:36:05 +0800

    feat: update voting component layout and content, enhance styles for better readability

--- src/views/vote/Vote.tsx
+++ src/views/vote/Vote.tsx
@@ -26,55 +26,39 @@ const Vote = () => {
         className={styles.card}
         title={
           <div className={styles.titleWrapper}>
-            <span className={styles.title}>AI聖誕卡創作投票規則</span>
+            <span className={styles.title}>AI 聖誕卡創作票選活動</span>
           </div>
         }
         // onBodyClick={onBodyClick}
         // onHeaderClick={onHeaderClick}
         style={{ "--adm-card-header-border-width": "0px" } as any}>
         <div className={styles.content}>
+          <div className={styles.subHeadTitle}>【 投票規則 】</div>
           <div className={styles.subTitle}>
-            歡迎大家參加聖誕卡片設計比賽投票活動！每位員工都可以參與這場充滿創意和節日氣氛的比賽，用你的票選出你心中的
-            No.1！投票將於 12 月 14 日上午 10 點 開跑，並在 12 月 14 日下午 3 點
-            截止，請投下寶貴的一票，讓我們一起為參賽者們加油吧！
+            豐藝集團 AI
+            聖誕卡片創作競賽，歡迎大家參加聖誕卡片設計比賽投票活動!用你的投票選出心中的No.1!!
           </div>
-
-          {/* <Image src='/qr-code/qr_code_RD007.png' fit='contain' /> */}
-          {/* 將 WaterMark 元素包裹在 Image 外層 */}
-          {/* <div className={styles.imageWrapper}>
-            {isUsed && (
-              <WaterMark
-                content={"票券已兌換"}
-                fontSize={30}
-                // image={"/qr-code/redeemed.png"}
-                // imageHeight={350}
-                // imageWidth={350}
-                // height={375}
-                // width={375}
-                fontColor='#a3a3a3'
-                fullPage={false}
-              />
-            )}
-            <Image src='/qr-code/qr_code_RD007.png' fit='contain' />
-          </div> */}
+          <div className={styles.subHeadTitle}>【 投票方式 】</div>
+          <div className={styles.subTitle}>
+            採取線上投票方式，只有參加今日豐藝集團家庭日員工者有投票權，每位員工可線上投{" "}
+            <span style={{ fontSize: "24px", fontWeight: "bold" }}>2</span>{" "}
+            張票（員工＋眷屬）。
+          </div>
+          <div className={styles.subHeadTitle}>【 投票時間 】</div>
+          <div className={styles.subTitle}>2024/12/14 09:00-15:00 截止投票</div>
         </div>
         <div className={styles.footer} onClick={(e) => e.stopPropagation()}>
           <Button
             color='primary'
             onClick={() => {
-              // Toast.show("活動尚未開放")
-              // setIsUsed(true)
-              window.location.href = "https://www.promate.com.tw/"
+              window.location.href = "https://supr.link/CtL4F"
             }}>
             點我觀看作品
           </Button>
           <Button
             color='primary'
             onClick={() => {
-              // Toast.show("活動尚未開放")
-              // setIsUsed(true)
-              // navigate()
-              window.location.href = "https://forms.gle/7cnsPHANkugNvHZZ8"
+              window.location.href = "https://supr.link/4kVj0"
             }}>
             點我進行投票
           </Button>
