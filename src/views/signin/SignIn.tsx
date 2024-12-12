abcedbcd33ff1dbfff6604806b17909f54c43005
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-12-13 00:09:15 +0800

    feat: refactor SignIn and Ticket components by removing unused code and adding loading state in Ticket component

--- src/views/signin/SignIn.tsx
+++ src/views/signin/SignIn.tsx
@@ -1,8 +1,7 @@
-import { Form, Input, Button, DotLoading } from "antd-mobile"
+import { Form, Input, Button } from "antd-mobile"
 import { MobileOutlined } from "@ant-design/icons"
 import styles from "./SignIn.module.less"
 import { useNavigate } from "react-router-dom"
-// import { useState } from "react"
 import Swal from "sweetalert2"
 import api from "../../api"
 import { Login, Result } from "../../types/api"
@@ -12,20 +11,7 @@ import { useStore } from "../../store"
 const SignIn = () => {
   const navigate = useNavigate()
   const MOBILE_LENGTH = 10
-  // const [loading, setLoading] = useState<boolean>(false)
   const { updateToken, updateCell } = useStore()
-
-  // useEffect(() => {
-  //   checkEmployeeAuth()
-  // }, [])
-
-  // const checkEmployeeAuth = async () => {
-  //   const token = storage.get("token")
-  //   if (token) {
-  //     navigate("/home")
-  //     console.log("Token exist:", token)
-  //   }
-  // }
 
   // Login function
   const onLoginFinished = async (values: Login.Params) => {
@@ -40,8 +26,6 @@ const SignIn = () => {
         timerProgressBar: true,
       })
     }
-
-    // setLoading(true)
 
     // Call the login API
     try {
@@ -72,8 +56,6 @@ const SignIn = () => {
         timer: 3000,
         timerProgressBar: true,
       })
-    } finally {
-      // setLoading(false)
     }
 
     //   console.log("onLoginFinished triggered", values)
@@ -118,7 +100,6 @@ const SignIn = () => {
 
   return (
     <>
-      <DotLoading />
       <div className={styles.wrapper}>
         {/* <div>
           <Image
--- src/views/ticket/Ticket.tsx
+++ src/views/ticket/Ticket.tsx
@@ -1,7 +1,184 @@
+// import { useState, useEffect } from "react"
+// import { useNavigate } from "react-router-dom"
+// import { useStore } from "../../store"
+// import { Card, Button, Divider } from "antd-mobile"
+// import api from "../../api"
+// import { Employee } from "../../types/api"
+// import storage from "../../utils/storage"
+// import Swal from "sweetalert2"
+// import styles from "../ticket/Ticket.module.less"
+
+// const Ticket = () => {
+//   const navigate = useNavigate()
+//   const [isChecked, setIsCheckd] = useState<boolean>(false)
+//   const { employeeInfo, updateEmployeeInfo } = useStore()
+
+//   useEffect(() => {
+//     checkEmployeeAuth()
+//   }, [])
+
+//   const checkEmployeeAuth = async () => {
+//     const token = storage.get("token")
+
+//     if (token) {
+//       await getEmployeeInfo()
+//     } else {
+//       navigate("/")
+//     }
+//   }
+
+//   const employeeCheckIn = async () => {
+//     const res: Employee.Info = await api.checkInEmployee(storage.getMobile())
+//     return res
+//   }
+
+//   const getEmployeeInfo = async () => {
+//     const res: Employee.Info = await api.getEmployeeInfo(storage.getMobile())
+//     setIsCheckd(res.is_checked)
+//     updateEmployeeInfo(res)
+//   }
+
+//   return (
+//     <div className={styles.wrapper}>
+//       <Card
+//         className={styles.card}
+//         title={
+//           <div className={styles.titleWrapper}>
+//             <span className={styles.title}>員工姓名 - {employeeInfo.name}</span>
+//           </div>
+//         }
+//         style={{ "--adm-card-header-border-width": "0px" } as any}>
+//         <div className={styles.content}>
+//           <div className={styles.subContent}>員工及眷屬人數</div>
+//           <div className={styles.participants}>
+//             <div className={styles.category}>
+//               {/* 成人 */}
+//               <div className={styles.categoryItem}>
+//                 <div className={styles.categoryItemTitle}>成人</div>
+//                 <div className={styles.categoryItemText}>
+//                   {employeeInfo.family_adult + employeeInfo.family_employee}
+//                 </div>
+//               </div>
+
+//               <Divider direction='vertical' />
+
+//               {/* 孩童、博愛 */}
+//               <div className={styles.categoryItem}>
+//                 <div className={styles.categoryItemTitle}>孩童、博愛</div>
+//                 <div className={styles.categoryItemText}>
+//                   {employeeInfo.family_child + employeeInfo.family_elderly}
+//                 </div>
+//               </div>
+
+//               <Divider direction='vertical' />
+
+//               {/* 幼童 */}
+//               <div className={styles.categoryItem}>
+//                 <div className={styles.categoryItemTitle}>幼童</div>
+//                 <div className={styles.categoryItemText}>
+//                   {employeeInfo.family_infant}
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+
+//           {/* <Image src='/qr-code/qr_code_RD007.png' fit='contain' /> */}
+//           {/* 將 WaterMark 元素包裹在 Image 外層 */}
+//           {/* <div className={styles.imageWrapper}>
+//             {isUsed && (
+//               <WaterMark
+//                 content={"票券已兌換"}
+//                 fontSize={30}
+//                 // image={"/qr-code/redeemed.png"}
+//                 // imageHeight={350}
+//                 // imageWidth={350}
+//                 // height={375}
+//                 // width={375}
+//                 fontColor='#a3a3a3'
+//                 fullPage={false}
+//               />
+//             )}
+//             <Image src='/qr-code/qr_code_RD007.png' fit='contain' />
+//           </div> */}
+//         </div>
+//         <div className={styles.footer} onClick={(e) => e.stopPropagation()}>
+//           <Button
+//             disabled={isChecked}
+//             color='primary'
+//             onClick={() => {
+//               Swal.fire({
+//                 title: "請輸入驗證碼兌換票券",
+//                 input: "password",
+//                 inputLabel: "請確實核對票券數量，兌換後恕不補發！",
+//                 inputAttributes: {
+//                   autocapitalize: "off",
+//                   autocomplete: "off",
+//                 },
+//                 icon: "question",
+//                 showCancelButton: true,
+//                 confirmButtonText: "確認",
+//                 cancelButtonText: "取消",
+//                 // preConfirm: async () => {
+//                 //   try {
+//                 //     const res = await employeeCheckIn()
+//                 //     if (res.is_checked) {
+//                 //       setIsCheckd(true)
+//                 //     }
+//                 //   } catch (error) {
+//                 //     console.error("Unexpected error:", error)
+//                 //   }
+//                 // },
+//               }).then(async (result) => {
+//                 console.log("result", result)
+
+//                 if (result.isConfirmed && result.value === "1214") {
+//                   try {
+//                     const ticket = await employeeCheckIn()
+//                     if (ticket.is_checked) {
+//                       setIsCheckd(true)
+//                       Swal.fire({
+//                         icon: "success",
+//                         text: "票券兌換成功！",
+//                         confirmButtonText: "知道了",
+//                       })
+//                     } else {
+//                       Swal.fire({
+//                         icon: "error",
+//                         text: "票券兌換失敗，請洽工作人員！",
+//                         confirmButtonText: "知道了",
+//                       })
+//                     }
+//                   } catch (error) {
+//                     console.error("Unexpected error:", error)
+//                     Swal.fire({
+//                       icon: "error",
+//                       text: "兌換失敗，請洽工作人員！",
+//                       confirmButtonText: "知道了",
+//                     })
+//                   }
+//                 } else if (result.isConfirmed && result.value !== "1214") {
+//                   Swal.fire({
+//                     icon: "error",
+//                     text: "驗證碼輸入錯誤，請重新輸入或洽工作人員！",
+//                     confirmButtonText: "知道了",
+//                   })
+//                 }
+//               })
+//             }}>
+//             {isChecked ? "已兌換票券" : "兌換票券"}
+//           </Button>
+//         </div>
+//       </Card>
+//     </div>
+//   )
+// }
+
+// export default Ticket
+
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useStore } from "../../store"
-import { Card, Button, Divider } from "antd-mobile"
+import { Card, Button, Divider, SpinLoading } from "antd-mobile"
 import api from "../../api"
 import { Employee } from "../../types/api"
 import storage from "../../utils/storage"
@@ -10,7 +187,8 @@ import styles from "../ticket/Ticket.module.less"
 
 const Ticket = () => {
   const navigate = useNavigate()
-  const [isChecked, setIsCheckd] = useState<boolean>(false)
+  const [isChecked, setIsChecked] = useState<boolean>(false)
+  const [loading, setLoading] = useState(true) // 頁面加載狀態
   const { employeeInfo, updateEmployeeInfo } = useStore()
 
   useEffect(() => {
@@ -33,9 +211,31 @@ const Ticket = () => {
   }
 
   const getEmployeeInfo = async () => {
-    const res: Employee.Info = await api.getEmployeeInfo(storage.getMobile())
-    setIsCheckd(res.is_checked)
-    updateEmployeeInfo(res)
+    try {
+      const res: Employee.Info = await api.getEmployeeInfo(storage.getMobile())
+      setIsChecked(res.is_checked)
+      updateEmployeeInfo(res)
+    } catch (error) {
+      console.error("Error fetching employee info:", error)
+      navigate("/") // 若發生錯誤，返回登入頁
+    } finally {
+      setLoading(false) // 結束加載
+    }
+  }
+
+  if (loading) {
+    // 在加載過程中顯示 DotLoading
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50vh",
+        }}>
+        <SpinLoading color='primary' style={{ "--size": "52px" }} />
+      </div>
+    )
   }
 
   return (
@@ -81,29 +281,10 @@ const Ticket = () => {
               </div>
             </div>
           </div>
-
-          {/* <Image src='/qr-code/qr_code_RD007.png' fit='contain' /> */}
-          {/* 將 WaterMark 元素包裹在 Image 外層 */}
-          {/* <div className={styles.imageWrapper}>
-            {isUsed && (
-              <WaterMark
-                content={"票券已兌換"}
-                fontSize={30}
-                // image={"/qr-code/redeemed.png"}
-                // imageHeight={350}
-                // imageWidth={350}
-                // height={375}
-                // width={375}
-                fontColor='#a3a3a3'
-                fullPage={false}
-              />
-            )}
-            <Image src='/qr-code/qr_code_RD007.png' fit='contain' />
-          </div> */}
         </div>
         <div className={styles.footer} onClick={(e) => e.stopPropagation()}>
           <Button
-            disabled={isChecked}
+            disabled={isChecked} // 已兌換時禁用按鈕
             color='primary'
             onClick={() => {
               Swal.fire({
@@ -118,24 +299,12 @@ const Ticket = () => {
                 showCancelButton: true,
                 confirmButtonText: "確認",
                 cancelButtonText: "取消",
-                // preConfirm: async () => {
-                //   try {
-                //     const res = await employeeCheckIn()
-                //     if (res.is_checked) {
-                //       setIsCheckd(true)
-                //     }
-                //   } catch (error) {
-                //     console.error("Unexpected error:", error)
-                //   }
-                // },
               }).then(async (result) => {
-                console.log("result", result)
-
                 if (result.isConfirmed && result.value === "1214") {
                   try {
                     const ticket = await employeeCheckIn()
                     if (ticket.is_checked) {
-                      setIsCheckd(true)
+                      setIsChecked(true)
                       Swal.fire({
                         icon: "success",
                         text: "票券兌換成功！",
